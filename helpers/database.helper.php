7945379a05f308ff2bc7c43c3fb9c501d212af33
Nanne Wielinga <nanne@mycel.nl>
2014-03-13 17:20:25 +0100

    database klasse uitgebreid

--- helpers/database.helper.php
+++ helpers/database.helper.php
@@ -2,19 +2,21 @@
 namespace Helpers;
 
 
+use PDO;
+use PDOException;
+
 class DatabaseHelper {
 
-    private static  $mysqlConn = NULL;
+    private static $mysqlConn = NULL;
+    private static $PDOConn = NULL;
+    private static $GISConn = NULL;
 
-    public static  function CreateDatabaseConnection($host, $username, $password, $database)
+    public static function CreateDatabaseConnection($host, $username, $password, $database)
     {
         $mysqli = @new \mysqli($host, $username, $password, $database);
-
-
-
+        $db = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
         if(mysqli_connect_errno())
         {
-
             trigger_error('[DatabaseHelper] New connection failed: '.@$mysqli->error);
             return false;
         }
@@ -23,13 +25,44 @@ class DatabaseHelper {
         {
             DatabaseHelper::$mysqlConn = $mysqli;
         }
+        if(DatabaseHelper::$PDOConn == null)
+        {
+            DatabaseHelper::$PDOConn = $db;
+        }
+        return true;
+    }
 
-        return $mysqli;
+    public static function CreateGISConnection($host, $username, $password, $database)
+    {
+
+        try {
+            $db = new PDO('pgsql:host='.$host.';dbname='.$database, $username, $password);
+            if(DatabaseHelper::$GISConn == null)
+            {
+                DatabaseHelper::$GISConn = $db;
+            }
+            return true;
+        } catch (PDOException $e) {
+            trigger_error('[DatabaseHelper] New connection failed: '.$e->getMessage());
+
+            return false;
+        }
+
     }
 
     public static function GetDefaultConnection()
     {
         return DatabaseHelper::$mysqlConn;
+    }
+
+    public static function GetPDOConnection()
+    {
+        return DatabaseHelper::$PDOConn;
+    }
+
+    public static function GetGISConnection()
+    {
+        return DatabaseHelper::$GISConn;
     }
 	
 	public static function Close()
