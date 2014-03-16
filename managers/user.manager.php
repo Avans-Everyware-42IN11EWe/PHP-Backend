0b3b850e73ad788e6c65a2fea43610cba7e09b52
eye2web <remcovanderheijden@hotmail.com>
2014-03-16 20:29:42 +0100

    Queries toegevoegd

--- managers/user.manager.php
+++ managers/user.manager.php
@@ -6,11 +6,37 @@ class UserManager
 
 	public static function Register($name, $email)
 	{	
+	
+		/*
+		// Test query
+		$qBuilder = new Builders\QueryBuilder("SELECT * FROM users");
+			
+		if($qBuilder->Execute())
+		{
+			$fetchedArray = $qBuilder->GetFetchedArray();
+			$qBuilder->Close();
+				
+			var_dump($fetchedArray);	
+	}*/
+	
 		echo array("response" => "ok", "token" => "TODO");
 	}
 	
 	public static function Login($token)
 	{	
+	
+			/*
+	// Test query
+	$qBuilder = new Builders\QueryBuilder("SELECT * FROM users");
+		
+	if($qBuilder->Execute())
+	{
+		$fetchedArray = $qBuilder->GetFetchedArray();
+		$qBuilder->Close();
+			
+		var_dump($fetchedArray);	
+	}*/
+	
 		echo array("response" => "ok", "token" => "TODO");
 	}
 
